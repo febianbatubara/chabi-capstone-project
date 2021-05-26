b01630963e593743089e4948fd2b43df75d43e9b
febianbatubara <febianbatubaraa@gmail.com>
2021-05-26 15:41:31 +0700

    add questionnaire and detail activity layout

---
+++ android/app/src/main/java/com/chabi/android/chabiapp/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.chabi.android.chabiapp.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.chabi.android.chabiapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ android/app/src/main/java/com/chabi/android/chabiapp/ui/questionnaire/QuestionnaireActivity.kt
@@ -0,0 +1,12 @@
+package com.chabi.android.chabiapp.ui.questionnaire
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.chabi.android.chabiapp.R
+
+class QuestionnaireActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_questionnaire)
+    }
+}
