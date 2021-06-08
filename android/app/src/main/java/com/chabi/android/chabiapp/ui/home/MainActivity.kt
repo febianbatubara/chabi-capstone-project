064c914c0d029edde61f392e00459d61d375e7a6
Abednego Sihombing <66824754+AbedSihombing@users.noreply.github.com>
2021-06-08 20:14:01 +0700

    merge

---
+++ android/app/src/main/java/com/chabi/android/chabiapp/ui/home/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.chabi.android.chabiapp.ui.home
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.chabi.android.chabiapp.R
+import com.chabi.android.chabiapp.adapter.RecyclerViewArticle
+import com.chabi.android.chabiapp.adapter.RecyclerViewVideo
+import com.chabi.android.chabiapp.databinding.ActivityHomeBinding
+import com.chabi.android.chabiapp.utils.Constant
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var article: RecyclerViewArticle
+    private lateinit var video: RecyclerViewVideo
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val prefs = getSharedPreferences(Constant.USER_PREF, Context.MODE_PRIVATE)
+        val username = prefs.getString(Constant.USER_NAME_KEY, "username")
+
+        binding.tvUsername.text = getString(R.string.hai_username, username)
+
+        binding.rvArticle.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        article = RecyclerViewArticle()
+        binding.rvArticle.adapter = article
+
+        binding.rvVideo.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        video = RecyclerViewVideo()
+        binding.rvVideo.adapter = video
+    }
+}
