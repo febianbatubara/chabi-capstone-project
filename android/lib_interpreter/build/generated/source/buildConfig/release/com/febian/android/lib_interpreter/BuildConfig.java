030c3fb5c6c4cc8eaaa378fc63b31781c8275acd
febianbatubara <febianbatubaraa@gmail.com>
2021-06-09 23:04:47 +0700

    add to main

---
+++ android/lib_interpreter/build/generated/source/buildConfig/release/com/febian/android/lib_interpreter/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.febian.android.lib_interpreter;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.febian.android.lib_interpreter";
+  public static final String BUILD_TYPE = "release";
+}
---
+++ android/lib_task_api/build/generated/source/buildConfig/release/com/febian/android/lib_task_api/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.febian.android.lib_task_api;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.febian.android.lib_task_api";
+  public static final String BUILD_TYPE = "release";
+}
